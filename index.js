23b16ee67188f0c662afd5fe71e390ad51cfbb78
Juan La Jara <wxomb@99.200-57-1.clientesbestel.com.mx>
2019-03-27 10:52:02 -0600

    Agregué ejemplo de navegación
    Precaución: esto no funciona al 100%

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ let cipher = {
   dos: 2,
   tres: 3,
 };
+
+window.onload = () => {
+  // Terminó de cargar el JS
+
+  // Cuál es el hash que estoy mostrando?
+  // => mostrar la sección correspondiente
+
+  if (window.location.pathname.split('#') === )
+};
